0da28ec43b188122b8f28dbd1a41aacc8f9b051d
Cole Brokamp <cole.brokamp@gmail.com>
2022-08-21 23:10:21 -0400

    starting to integrate metdata code with data code; using CODECtools package

--- 01_make_hh_acs_data.R
+++ 01_make_hh_acs_data.R
@@ -4,7 +4,6 @@ options(tigris_use_cache = TRUE)
 options(tigris_class = "sf")
 library(tidycensus)
 library(purrr)
-
 library(CODECtools)
 
 # TODO run check for census api key
@@ -50,12 +49,8 @@ cpi <- cpi |>
   mutate(annual_cpi_2010 = subset(cpi, year == 2010)$annual_cpi) |>  # in 2010 inflation-adjusted dollars
   mutate(ratio = annual_cpi_2010 / annual_cpi)
 
-
 d_acs <- list()
 
-
-# fraction_poverty
-# income in past 12 months below poverty level
 get_acs_poverty <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -73,11 +68,12 @@ get_acs_poverty <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_poverty <- mappp_dfr(2010:2019, get_acs_poverty)
+d_acs$acs_poverty <-
+  mappp_dfr(2010:2019, get_acs_poverty) |>
+  add_col_attrs(fraction_poverty,
+                title = "Fraction of Households in Poverty",
+                description = "Fraction of households with income below poverty level within the past 12 months")
 
-
-# number of children under age 18
-# total population
 get_acs_children <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -101,11 +97,16 @@ get_acs_children <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_children <- mappp_dfr(2010:2019, get_acs_children)
+d_acs$acs_children <-
+  mappp_dfr(2010:2019, get_acs_children) |>
+  add_col_attrs(n_children_lt18,
+    title = "Number of Children",
+    description = "Number of children and adolescents < 18 years of age"
+  ) |>
+  add_col_attrs(n_pop,
+    title = "Number of Total People"
+    )
 
-
-# number of household with children under age 18
-# total household
 get_acs_households <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -125,10 +126,16 @@ get_acs_households <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_households <- mappp_dfr(2010:2019, get_acs_households)
+d_acs$acs_households <-
+  mappp_dfr(2010:2019, get_acs_households) |>
+  add_col_attrs(n_household_lt18,
+    title = "Number of Households With Children",
+    description = "Number of households with children or adolescents < 18 years of age"
+    ) |>
+  add_col_attrs(n_household,
+    title = "Number of Households"
+  )
 
-
-# fraction insured (2012 onwards only)
 get_acs_insurance <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -157,10 +164,13 @@ get_acs_insurance <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_insurance <- mappp_dfr(2012:2019, get_acs_insurance) 
-
-
-# fraction received food stamps or snap
+d_acs$acs_insurance <-
+  mappp_dfr(2012:2019, get_acs_insurance) |>
+  add_col_attrs(fraction_insured,
+    title = "Fraction of People Insured",
+    description = "Fraction of population with health insurance (available from 2012 onwards only)"
+  )
+  
 get_acs_snap <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -174,14 +184,17 @@ get_acs_snap <- function(year = 2019) {
       fraction_snap = estimate / summary_est,
       fraction_snap_moe = moe_prop(estimate, summary_est, moe, summary_moe)
     )
-  left_join(tracts_needed, d, by = "census_tract_id_2010") |> 
+  left_join(tracts_needed, d, by = "census_tract_id_2010") |>
     mutate(year = year)
 }
 
-d_acs$acs_snap <- mappp_dfr(2010:2019, get_acs_snap)
+d_acs$acs_snap <-
+  mappp_dfr(2010:2019, get_acs_snap) |>
+  add_col_attrs(fraction_snap,
+    title = "Fraction of Households Receiving SNAP",
+    description = "Fraction of households receiving food stamps/SNAP in the past 12 months"
+  )
 
-
-# fraction family household with no spouse
 get_acs_hh_type <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -199,9 +212,13 @@ get_acs_hh_type <- function(year = 2019) {
     mutate(year = year)
 }
     
-d_acs$acs_hh_type <- mappp_dfr(2010:2019, get_acs_hh_type)
-
-
+d_acs$acs_hh_type <-
+  mappp_dfr(2010:2019, get_acs_hh_type) |>
+  add_col_attrs(fraction_fam_nospouse,
+    title = "Fraction of family households with a single householder",
+    description = "Single householder is male or female household, with no spouse present"
+  )
+                
 get_acs_employment <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -226,9 +243,7 @@ d_acs$acs_employment <-
     description = "Fraction of people employed in civilian labor force"
   )
 
-
-# median monthly housing cost
-get_acs_housingcost <- function(year = 2019) {
+get_acs_housing_cost <- function(year = 2019) {
   d <-
     my_get_acs(
       variables = "B25105_001",
@@ -237,21 +252,27 @@ get_acs_housingcost <- function(year = 2019) {
     suppressMessages() |>
     transmute(
       census_tract_id_2010 = GEOID,
-      median_housingcost = estimate,
-      median_housingcost_moe = moe
+      median_housing_cost = estimate,
+      median_housing_cost_moe = moe
     )
   left_join(tracts_needed, d, by = "census_tract_id_2010") |> 
     mutate(year = year)
 }
 
-d_acs$acs_housingcost <- mappp_dfr(2010:2019, get_acs_housingcost) |> 
-  left_join(cpi, by = "year") |> 
-  mutate(median_housingcost_2010adj = median_housingcost * ratio) |> 
-  select(-annual_cpi, -annual_cpi_2010, -ratio)
+d_acs$acs_housing_cost <-
+  mappp_dfr(2010:2019, get_acs_housing_cost) |>
+  left_join(cpi, by = "year") |>
+  mutate(median_housing_cost_2010adj = median_housing_cost * ratio) |>
+  select(-annual_cpi, -annual_cpi_2010, -ratio) |>
+  add_col_attrs(median_housing_cost,
+    title = "Median Monthly Housing Costs",
+    description = "Median monthly housing costs in 2010-adjusted dollars"
+  )
 
+# TODO B25106: Tenure by Housing Costs as a Percentage of Household Income
+# fraction of renter households making less than $35k/year and spending at least 30% of income on housing costs??
 
-# median gross rent
-get_acs_grossrent <- function(year = 2019) {
+get_acs_rent <- function(year = 2019) {
   d <-
     my_get_acs(
       variables = "B25113_001",
@@ -260,21 +281,25 @@ get_acs_grossrent <- function(year = 2019) {
     suppressMessages() |>
     transmute(
       census_tract_id_2010 = GEOID,
-      median_grossrent = estimate,
-      median_grossrent_moe = moe
+      median_rent = estimate,
+      median_rent_moe = moe
     )
   left_join(tracts_needed, d, by = "census_tract_id_2010") |> 
     mutate(year = year)
 }
 
-d_acs$acs_grossrent <- mappp_dfr(2010:2019, get_acs_grossrent) |> 
-  left_join(cpi, by = "year") |> 
-  mutate(median_grossrent_2010adj = median_grossrent * ratio) |> 
-  select(-annual_cpi, -annual_cpi_2010, -ratio)
+d_acs$acs_rent <-
+  mappp_dfr(2010:2019, get_acs_rent) |>
+  left_join(cpi, by = "year") |>
+  mutate(median_rent_2010adj = median_rent * ratio) |>
+  select(-annual_cpi, -annual_cpi_2010, -ratio) |>
+  add_col_attrs(median_rent,
+    title = "Median Monthly Rent",
+    description = "Median monthly rent in 2010-adjusted dollars"
+  )
 
 
-# fraction of tenure with selected physical and financial condition
-get_acs_tenure <- function(year = 2019) {
+get_acs_conditions <- function(year = 2019) {
   d <-
     my_get_acs(
       variables = c(
@@ -288,24 +313,28 @@ get_acs_tenure <- function(year = 2019) {
     suppressMessages() |>
     group_by(GEOID) |>
     summarize(
-      n_tenure_cond = sum(estimate),
-      n_tenure_cond_moe = moe_sum(moe, estimate),
+      n_conditions = sum(estimate),
+      n_conditions_moe = moe_sum(moe, estimate),
       n_total = unique(summary_est),
       n_total_moe = unique(summary_moe)
     ) |>
     transmute(
       census_tract_id_2010 = GEOID,
-      fraction_tenure_cond = n_tenure_cond / n_total,
-      fraction_tenure_cond_moe = moe_prop(n_tenure_cond, n_total, n_tenure_cond_moe, n_total_moe)
+      fraction_conditions = n_conditions / n_total,
+      fraction_conditions_moe = moe_prop(n_conditions, n_total, n_conditions_moe, n_total_moe)
     )
   left_join(tracts_needed, d, by = "census_tract_id_2010") |> 
     mutate(year = year)
 }
 
-d_acs$acs_tenure <- mappp_dfr(2010:2019, get_acs_tenure)
+d_acs$acs_conditions <-
+  mappp_dfr(2010:2019, get_acs_conditions) |>
+  add_col_attrs(fraction_conditions,
+    title = "Fraction of households with substandard housing conditions",
+    description = "substandard housing: incomplete plumbing or kitchens, overcrowding, 30% or more of household income spent on rent or owner costs"
+  )
 
 
-# fraction of structure built before 1970
 get_acs_yrbuilt <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -333,10 +362,13 @@ get_acs_yrbuilt <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_yrbuilt <- mappp_dfr(2010:2019, get_acs_yrbuilt)
+d_acs$acs_yrbuilt <-
+  mappp_dfr(2010:2019, get_acs_yrbuilt) |>
+  add_col_attrs(fraction_builtbf1970,
+    name = "Fraction of Homes Built Before 1970"
+  )
 
 
-# fraction of vacant housing
 get_acs_occup <- function(year = 2019) {
   d <-
     my_get_acs(
@@ -354,7 +386,11 @@ get_acs_occup <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_occup <- mappp_dfr(2010:2019, get_acs_occup)
+d_acs$acs_occup <-
+  mappp_dfr(2010:2019, get_acs_occup) |>
+  add_col_attrs(fraction_vacant,
+    title = "Fraction of housing units that are vacant"
+  )
 
 
 # racial/ethnic composition - non-Hispanic or Latino, total
@@ -375,7 +411,11 @@ get_acs_fraction_nhl <- function(year = 2019) {
     mutate(year = year)
 }
 
-d_acs$acs_fraction_nhl <- mappp_dfr(2010:2019, get_acs_fraction_nhl)
+d_acs$acs_fraction_nhl <-
+  mappp_dfr(2010:2019, get_acs_fraction_nhl) |>
+  add_col_attrs(fraction_nhl,
+    title = "Fraction of People that are non-Hispanic or Latino"
+  )
 
 
 # racial/ethnic composition - non-Hispanic or Latino, White
@@ -625,142 +665,27 @@ d <- select(d, -contains("_moe"))
 
 # saveRDS(d, "data/harmonized_historical_acs_data.rds")
 
-d |>
-  group_by(year) |>
-  summarize(n = n())
-
-source("00_helpers.R")
-
 d <- d |>
-  set_attrs(
-    name = "harmonized_historical_acs_data",
-    path = "s3://geomarker-io/data/harmonized_historical_ACS_data.csv",
-    # TODO how to utilize object versioning on AWS S3
-    ## version = "0.1",
-    title = "Harmonized Historical American Community Survey Data",
-    description = "ACS variables from 2010 - 2019, census tracts for contiguous US",
-    format = "csv",
-    url = "https://github.com/geomarker-io/harmonized_historical_ACS_data",
-    # TODO change these to use foreign keys specification
-    codec.spatial = "census_tract_id_2010",
-    codec.temporal = "year"
-  )
-
-d
-attributes(d)
-attr(d, "name")
-attr(d, "path")
-
-d <- d |>
-  set_col_attrs(census_tract_id_2010,
+  add_col_attrs(census_tract_id_2010,
     title = "Census Tract Identifier (2010)",
     description = "FIPS identifier for census tracts 2010 - 2019"
   ) |>
-  set_col_attrs(n_children_lt18,
-    title = "Number of Children",
-    description = "Number of children and adolescents < 18 years of age"
-  ) |>
-  set_col_attrs(year,
+  add_col_attrs(year,
     title = "Year",
     description = "Vintage of 5-year ACS estimates"
-  ) |>
-  set_col_attrs(n_household_lt18,
-    title = "Number of Households with Children"
-  ) |>
-  set_col_attrs(n_household,
-    title = "Number of Households"
-  ) |>
-  set_col_attrs(fraction_insured,
-    title = "Fraction Population with Health Insurance"
-  ) |>
-  set_col_attrs(fraction_poverty,
-    title = "Fraction of Households in Poverty"
-    ) |>
-  set_col_attrs(n_pop,
-    title = "Number of Total People"
-    ) |>
-  set_col_attrs(fraction_snap,
-    title = "Fraction of Households Receiving Food Stamps or SNAP Dollars"
-  ) |>
-  set_col_attrs(fraction_fam_nospouse,
-                title = "Fraction of Households with No Spouse Present"
-  ) |>
-  set_col_attrs(fraction_laborforce,
-                title = "Fraction of Population 16 Years and Over in Labor Force"
-  )  |>
-  set_col_attrs(median_housingcost,
-                title = "Median Monthly Housing Costs"
-  ) |>
-  set_col_attrs(median_housingcost_2010adj,
-                title = "Median Monthly Housing Costs (Inflation Adjusted)"
-  ) |>
-  set_col_attrs(median_grossrent,
-                title = "Median Gross Rent by Year Householder Moved into Unit"
-  ) |>
-  set_col_attrs(median_grossrent_2010adj,
-                title = "Median Gross Rent by Year Householder Moved into Unit (Inflation Adjusted)"
-  ) |>
-  set_col_attrs(fraction_tenure_cond,
-                title = "Occupied Housing Units with One or More Selected Physical and Financial Conditions"
-  ) |>
-  set_col_attrs(fraction_builtbf1970,
-                title = "Fraction of Housing Units Built Before 1970",
-                description = "The variable represents housing stock correlated with high risk of elevated blood level concentrations in children"
-  ) |>
-  set_col_attrs(fraction_vacant,
-                title = "Fraction of Vacant Housing Units"
-  ) |>
-  set_col_attrs(fraction_nhl,
-                title = "Fraction of Non Hispanic or Latino (Total All Races)"
-  ) |>
-  set_col_attrs(fraction_nhl_w,
-                title = "Fraction of Non Hispanic or Latino, White"
-  ) |>
-  set_col_attrs(fraction_nhl_b,
-                title = "Fraction of Non Hispanic or Latino, Black"
-  ) |>
-  set_col_attrs(fraction_nhl_o,
-                title = "Fraction of Non Hispanic or Latino, Any Other Race (Including Two or More Races)"
-  ) |>
-  set_col_attrs(fraction_hl,
-                title = "Fraction of Fraction of Hispanic or Latino (Total All Races)"
-  ) |>
-  set_col_attrs(fraction_hl_w,
-                title = "Fraction of Hispanic or Latino, White"
-  ) |>
-  set_col_attrs(fraction_hl_b,
-                title = "Fraction of Hispanic or Latino, Black"
-  ) |>
-  set_col_attrs(fraction_hl_o,
-                title = "Fraction of Hispanic or Latino, Any Other Race (Including Two or More Races)"
-  ) |>
-  set_col_attrs(fraction_leshh,
-                title = "Fraction of Limited English Speaking Households"
-  ) |>
-  set_col_attrs(median_income,
-                title = "Median Household Income in the Past 12 Months"
-  ) |>
-  set_col_attrs(median_income_2010adj,
-                title = "Median Household Income in the Past 12 Months (2010 Inflation-Adjusted Dollars)"
-  ) |>
-  set_col_attrs(fraction_hs,
-                title = "Fraction of Population 25 and Older with Education Attainment of at Least High School Graduate"
   )
 
-## add type (from the Frictionless Table Schema) to each column based on its class
+d <- d |>
+  add_attrs(
+    name = "harmonized_historical_acs_data",
+    path = "harmonized_historical_ACS_data.csv",
+    title = "Harmonized Historical American Community Survey Data",
+    description = "ACS variables from 2010 - 2019, census tracts for contiguous US",
+    url = "https://github.com/geomarker-io/harmonized_historical_ACS_data",
+    ) |>
+  add_type_attrs()
 
-d <- add_type_attrs(d)
+get_descriptors(d)
+get_schema(d)
 
-## a quick visual check of metadata in a browser
-CB::ltable(make_metadata_from_attr(d))
-
-# write metadata to file
-write_metadata(d, "datapackage.yaml")
-
-# write data to CSV file
-readr::write_csv(d, "harmonized_historical_acs_data.csv")
-
-# TODO how to upload to s3 location based on metadata$path?
-
-# TODO special case of 2020 where census tracts ids are different
-
+write_tdr_csv(d)
